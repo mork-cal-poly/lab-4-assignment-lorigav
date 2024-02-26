b9d32b42b37370a1534ca664c2c369b44d635138
Lori Gavia <lj40709@gmail.com>
2024-02-25 22:51:28 -0800

    drawrCreature1: Add Function
    added drawCreature1 function from lab 1

--- src/sketch.js
+++ src/sketch.js
@@ -9,4 +9,46 @@ function setup() {
 
 function draw() {
   background(220);
+  drawCreature1(200, 200)
+}
+
+function drawCreature1(x,y){
+  push();
+    translate(x, y);
+      //body
+      fill('#94491E')
+      rect(-95,-80,190,150,50,50,40,40)
+      fill('#6B3515')
+      triangle(10,-80,-10,-80,50,-150)
+      //head
+      fill('#94491E')
+      rect(-75,-50,150,100,20,20,50,50)
+      //paws
+      ellipse(45,65,70,40)
+      ellipse(-45,65,70,40)
+      //mouth&nose
+      fill('#EFBBA3')
+      ellipse(0,5,60,40)
+      fill(0)
+      ellipse(0,0,20,10)
+      strokeWeight(2)
+      line(0,0,0,15)
+      line(0,15,-10,23)
+      line(0,15,10,23)
+      //eyes
+      fill(0)
+      circle(40,-25,16)
+      circle(-40,-25,16)
+      fill(255)
+      quad(40,-30,45,-25,40,-20,35,-25)
+      quad(-40,-30,-45,-25,-40,-20,-35,-25)
+      fill('#6B3515')
+      strokeWeight(1)
+      arc(40,-35,25,15,0,-PI, CHORD)
+      arc(-40,-35,25,15,0,-PI, CHORD)
+      //ears
+      fill('#6B3515')
+      triangle(-75,-35,-75,75,-110,35)
+      triangle(75,-35,75,75,110,35)
+    pop();
 }
